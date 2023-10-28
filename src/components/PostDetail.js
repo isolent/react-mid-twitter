dbc323faa83b34eb1262ff08aaae67f72c1614c0
isolent <a_kakharmanova@kbtu.kz>
2023-10-28 16:05:48 +0600

    midterm

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,104 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { fetchPostById } from '../mockApi';
-import "./PostDetail";
+// PostDetail.js
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function PostDetail() {
-  const { id } = useParams();
-  const [post, setPosts] = useState(null);
-  const [newComment, setNewComment] = useState('');
-  const [editingCommentIndex, setEditingCommentIndex] = useState(-1);
-  const [comments, setComments] = useState([]); // Добавьте состояние для комментариев
-
-  // Функция для добавления комментария
-  const addComment = (comment) => {
-    setComments([...comments, comment]);
-  };
-
-  // Функция для обновления комментария
-  const updateComment = (index, comment) => {
-    const updatedComments = [...comments];
-    updatedComments[index] = comment;
-    setComments(updatedComments);
-  };
-
-  // Функция для удаления комментария
-  const deleteComment = (index) => {
-    const updatedComments = [...comments];
-    updatedComments.splice(index, 1);
-    setComments(updatedComments);
-  };
-
-  const handleAddComment = () => {
-    if (newComment) {
-      addComment(newComment);
-      setNewComment('');
-    }
-  };
-
-  const handleEditComment = (index) => {
-    setEditingCommentIndex(index);
-    setNewComment(comments[index]);
-  };
-
-  const handleUpdateComment = () => {
-    if (newComment) {
-      updateComment(editingCommentIndex, newComment);
-      setEditingCommentIndex(-1);
-      setNewComment('');
-    }
-  };
-
-
-  useEffect(() => {
-    fetchPostById(parseInt(id, 10))
-      .then((data) => {
-        setPosts(data);
-      })
-      .catch((error) => {
-        console.error('Error loading post data:', error);
-      });
-  }, [id]);
-
-  if (!post) {
-    return <div>The post was not found.</div>;
-  }
-
-return (
-    <div className='main'>
-      <div>
-        <div className='information'>
-          <p>{post.name}</p> 
-          <p>{post.description}</p>
-        </div>
-
-       <div className='line'>
-        <input
-          type="text"
-          placeholder="Add a comment..."
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-          className='add-comment'
-        />
-        <button className='but' onClick={editingCommentIndex === -1 ? handleAddComment : handleUpdateComment}>
-          {editingCommentIndex === -1 ? 'Send' : 'Update'}
-        </button>
-      </div>
-
-      <h3>Comments</h3>
-        {comments.map((comment, index) => (
-          <div key={index} className="comment-box">
-            <p> <strong> {comment} </strong> </p>
-            <div className='deled'>
-            <button onClick={() => deleteComment(index)}>Delete</button>
-            <button onClick={() => handleEditComment(index)}>Edit</button>
-          </div>
-          </div>
-        ))}
-
-      </div>
- </div>
-);
+const PostDetail = ({ post }) => {
+  return (
+    <div>
+      <Link to="/">Back to Post List</Link>
+      <h1>Post Detail</h1>
+      <h3>{post.text}</h3>
+      <p>{post.body}</p>
+    </div>
+  );
 };
 
 export default PostDetail;
